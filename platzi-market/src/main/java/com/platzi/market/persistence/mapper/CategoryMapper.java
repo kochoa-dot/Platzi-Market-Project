a5463b5067eb24d4011d98f63b79c76e03cda892
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-09-08 00:05:56 -0600

    creating the mappers for products and category to convert those with mapperstruct

---
+++ platzi-market/src/main/java/com/platzi/market/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration//indica que debe hacer el mapeo inverso al que ya tenemos
+    @Mapping(target = "productos", ignore = true)//Indicamos que este atributo no irá y que lo debe ignorar
+    Categoria toCategoria(Category category);
+}
